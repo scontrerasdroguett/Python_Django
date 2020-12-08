5f1996f2d41d26cf9d621b83396b189bd5289364
scontrerasdroguett <sca.contreras@alumnos.duoc.cl>
2020-12-08 15:35:58 -0300

    Paginador y ejemplos
    Se ha agregado el paginador en las apps de Rescatados y Blog, ademas de agregar ejemplos en ambas apps

--- sitios/adopta_me/rescatados/views.py
+++ sitios/adopta_me/rescatados/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from .models import Rescatado
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def rescatados(request):
     rescatados = Rescatado.objects.all()
+
+    page = request.GET.get('page',1)
+    paginator = Paginator(rescatados, 5)
+
+    try:
+        rescatados = paginator.page(page)
+    except PageNotAnInteger:
+        rescatados = paginator.page(1)
+    except EmptyPage:
+        rescatados = paginator.page(paginator.num_pages)
+
     return render(request, "rescatados/rescatados.html", {'rescatados':rescatados})
